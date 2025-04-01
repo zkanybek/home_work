63137f1a6d86dbee7d52411ca71681e571b7bce2
zkanybek <zkanybekov62@gmail.com>
2025-04-01 13:11:53 +0600

    LS: addad lesson1

---
+++ home_work.py
@@ -0,0 +1,27 @@
+class Hero:
+
+    def __init__(self, name, lvl, hp):
+        self.name = name
+        self.lvl = lvl
+        self.HP = hp
+
+    def introduc(self):
+        print(f'Привет, меня зовут {self.name}, мой lvl {self.lvl} , мое HP {self.HP}')
+    def is_adult(self):
+        if 10 <= self.lvl:
+            return True
+        else:
+            return False
+
+
+zk = Hero('zkanybek', 1, 100)
+son = Hero('Son', 100, 1700)
+black = Hero('Knight',9, 190 )
+
+print(zk.introduc())
+print(son.introduc())
+print(black.introduc())
+
+print(zk.is_adult())
+print(son.is_adult())
+print(black.is_adult())
---
+++ lesson1.py
@@ -0,0 +1,18 @@
+from tkinter.font import names
+
+
+class Person:
+
+    def __init__(self, name, age, city):
+        self.name = name,
+        self.age = age,
+        self.city = city,
+
+    def introduc(self):
+        print(f'Привет меня зовут {self.name}')
+
+
+
+
+
+
